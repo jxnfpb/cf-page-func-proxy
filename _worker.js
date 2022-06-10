a2a185a6f06e051dd687884a64dc97f9f1ee8d57
jxnfpb <96660889+jxnfpb@users.noreply.github.com>
2022-06-10 17:38:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
+        url.hostname=" https://github.com/xyTom/cf-page-func-proxy/blob/291f504f9be4a65d2daa39a954a41bd8088561f3/_worker.js#L5";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
