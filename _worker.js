0ecd24056f41994775054d5a8c194096afc725c6
jxnfpb <96660889+jxnfpb@users.noreply.github.com>
2022-06-11 22:21:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,21 @@
 export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
-    }
-  };
+async fetch(request, env) {
+const cars = [
+"app1.example.com",
+"app2.example.com",
+"app3.example.com",
+"app4.example.com",
+"app5.example.com"
+];
+let host = cars[Math.floor(Math.random() * cars.length)]; //随机选择VPS
+//let host = cars[new Date().getDate() % cars.length]; //每天自动更换VPS
+
+let url = new URL(request.url);
+if (url.pathname.startsWith('/')) {
+url.hostname = host;
+let new_request = new Request(url,request);
+return fetch(new_request);
+}
+return env.ASSETS.fetch(request);
+}
+};
